47bd80d0c73a28f7dd37bdf43e94915da3b75ff4
Avolventa <tatianamedvedeva27@gmail.com>
2017-11-19 12:36:43 +0300

    Merge sort

--- src/main/java/edu/spbu/matrix/DenseMatrix.java
+++ src/main/java/edu/spbu/matrix/DenseMatrix.java
@@ -8,25 +8,68 @@ import java.io.*;
  */
 public class DenseMatrix implements Matrix
 {
-  public int size;
-  public double matrix[][];
+  public int denseSize;
+  public double denseMatrix[][];
   /**
    * загружает матрицу из файла
    * @param fileName
    */
   public DenseMatrix(String fileName) {
-    //открыть файл
-    File in;
     try {
-      in = new File(fileName);
-    } catch(FileNotFoundException e) {
-      System.out.println("Файл не найден");
+      //open "in" file
+      FileInputStream in;
+      try {
+        in = new FileInputStream(fileName);
+      } catch (FileNotFoundException e) {
+        System.out.println("In file not found");
+        return;
+      }
+      //getting size from the first row
+      String[] firstRow;
+      Scanner scan = new Scanner(in);
+      firstRow = (scan.nextLine()).split(" ");
+      denseSize = (firstRow).length;
+      denseMatrix = new double[denseSize][denseSize];
+      //recording the first row
+      for (int i = 0; i < denseSize; i++) {
+        denseMatrix[0][i] = Double.parseDouble(firstRow[i]);
+      }
+      //recording the other rows
+      for (int i = 1; i < denseSize; i++) {
+        for (int j = 0; j < denseSize; j++) {
+          if(scan.hasNextDouble()) {
+            denseMatrix[i][j] = scan.nextDouble();
+          } else {
+            System.out.println("The wrong format of number");
+          }
+        }
+      }
+      //close file
+      in.close();
+    } catch (Exception e) {
+      System.out.println("Something is wrong");
       return;
     }
-    Scanner sc = new Scanner(in);
-    size = (sc.nextLine()).split(" ").length;
-    //in.close();
-    //закрыть файл
+  }
+
+  public void printMatrix(String fileName) {
+    try {
+      //open "out" file
+      try {
+        FileOutputStream fout = new FileOutputStream(fileName);
+        //Recording in file
+        for (int i = 0; i < denseSize; i++) {
+          for (int j = 0; j < denseSize; j++) {
+            fout.write((String.valueOf(denseMatrix[i][j]) + " ").getBytes());
+          }
+          fout.write(("\r\n").getBytes());
+        }
+      } catch (FileNotFoundException e) {
+        System.out.println("Out file was not found");
+      }
+    } catch (Exception e) {
+      System.out.println("Error in method printMatrix");
+    }
   }
   /**
    * однопоточное умнджение матриц
--- src/main/java/edu/spbu/matrix/MatrixMain.java
+++ src/main/java/edu/spbu/matrix/MatrixMain.java
@@ -1,8 +1,17 @@
 package edu.spbu.matrix;
 
+import java.io.IOException;
+
 public class MatrixMain {
     public static void main(String args[]) {
-        DenseMatrix DMatrix = new DenseMatrix("src/main/DenseFirst.txt");
-        System.out.println(DMatrix.size);
+        try {
+            DenseMatrix DMatrix = new DenseMatrix("src/main/DenseFirst.txt");
+            System.out.println(DMatrix.denseSize);
+
+            DMatrix.printMatrix("src/main/DenseOut.txt");
+        } catch (Exception e) {
+            System.out.println("Error in main");
+            return;
+        }
     }
 }
