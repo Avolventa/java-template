8f0dac4b553f1e874f0d800d0e29a37969b70d3b
Avolventa <tatianamedvedeva27@gmail.com>
2017-11-23 01:04:47 +0300

    Merge sort

--- src/test/java/edu/spbu/matrix/MatrixTest.java
+++ src/test/java/edu/spbu/matrix/MatrixTest.java
@@ -11,9 +11,33 @@ public class MatrixTest
    */
   @Test
   public void mulDD() {
-    Matrix m1 = new DenseMatrix("m1.txt");
-    Matrix m2 = new DenseMatrix("m2.txt");
-    Matrix expected = new DenseMatrix("result.txt");
-    assertEquals(expected, m1.mul(m2));
+    Matrix d1 = new DenseMatrix("d1.txt");
+    Matrix d2 = new DenseMatrix("d2.txt");
+    Matrix expected = new DenseMatrix("resultDD.txt");
+    assertEquals(expected, d1.mul(d2));
+  }
+
+  @Test
+  public void mulSD() {
+    Matrix s1 = new SparseMatrix("s1.txt");
+    Matrix d2 = new DenseMatrix("d2.txt");
+    Matrix expected = new DenseMatrix("resultSD.txt");
+    assertEquals(expected, s1.mul(d2));
+  }
+
+  @Test
+  public void mulDS() {
+    Matrix d1 = new DenseMatrix("d1.txt");
+    Matrix s2 = new SparseMatrix("s2.txt");
+    Matrix expected = new DenseMatrix("resultDS.txt");
+    assertEquals(expected, d1.mul(s2));
+  }
+
+  @Test
+  public void mulSS() {
+    Matrix s1 = new SparseMatrix("s1.txt");
+    Matrix s2 = new SparseMatrix("s2.txt");
+    Matrix expected = new DenseMatrix("resultSS.txt");
+    assertEquals(expected, s1.mul(s2));
   }
 }
